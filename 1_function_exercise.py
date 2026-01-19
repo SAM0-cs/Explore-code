e8ebf3db86d210d7d7fea187de0c8c061b5c3d49
SAM0-cs <malavisiddheshwar@gmail.com>
2026-01-19 21:39:32 +0530

    Create 1_function_exercise.py

---
+++ 1_function_exercise.py
@@ -0,0 +1,28 @@
+def calculate_grade(score):
+    if score >= 90:
+        return "A"
+    elif score >= 75:
+        return "B"
+    elif score >= 60:
+        return "C"
+    elif score >= 40:
+        return "D"
+    else:
+        return "F"
+    
+def generate_student_report(name,score):
+    grade = calculate_grade(score)
+    return (f"{name.upper()} has scored {score} marks and get grade '{grade}'")
+
+
+names = ["sam","ram","arjun","tejas","akya","abhi"]
+marks = [99,88,77,66,55,44]
+
+result=[]
+for i in range(len(names)):
+       msg = generate_student_report(names[i],marks[i])
+       result.append(msg)
+
+for msg in result :
+    print(msg)
+    print()
